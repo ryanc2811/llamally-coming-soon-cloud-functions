3a359279a60b1943904239630dd3057997a30dc7
Ryan Coles <ryanc2811@live.co.uk>
2023-05-24 11:19:30 +0100

    added updateRank function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,37 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+interface Player {
+    id: string;
+    email: string;
+    displayName: string;
+    score: number;
+    rank?: number;
+}
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const updateRank = functions.region('europe-west2').firestore
+    .document('highscores/{documentId}')
+    .onWrite(async (_change, _context) => {
+        const batch = admin.firestore().batch();
+        const playersSnapshot = await admin.firestore().collection('highscores').get();
+
+        let players: Player[] = [];
+        playersSnapshot.forEach(doc => {
+            players.push({ id: doc.id, ...doc.data() } as Player);
+        });
+
+        players.sort((a, b) => b.score - a.score);
+
+        for(let i = 0; i < players.length; i++) {
+            let player = players[i];
+            let rank = i + 1;  // rank is index + 1
+            let playerDocRef = admin.firestore().collection('highscores').doc(player.id);
+            batch.update(playerDocRef, { rank: rank });
+        }
+
+        return batch.commit()
+            .then(() => console.log('Successfully updated ranks'))
+            .catch(error => console.error('Failed to update ranks:', error));
+    });
